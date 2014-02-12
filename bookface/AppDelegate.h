90a23f168d0425d92a3e21dc8c3279205c631bce
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-02-11 21:10:59 -0800

    Initial Commit

---
+++ bookface/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bookface
+//
+//  Created by Joshua Dickens on 2/11/14.
+//  Copyright (c) 2014 Joshua Dickens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
