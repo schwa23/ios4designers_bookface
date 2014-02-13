1364f7e599898001b4dce4522904931be5562b52
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-02-12 21:23:58 -0800

    Textfield in toolbar responds to input, shows and hides the keyboard, and animates in.

--- bookface/NewsPostViewController.m
+++ bookface/NewsPostViewController.m
@@ -15,11 +15,18 @@
 @property (weak, nonatomic) IBOutlet UIImageView *tabbar;
 @property (weak, nonatomic) IBOutlet UIView *postContent;
 @property (weak, nonatomic) IBOutlet UIImageView *postImage;
+@property (weak, nonatomic) IBOutlet UIToolbar *commentToolbar;
+@property (weak, nonatomic) UITextField *commentField;
+
+
+- (IBAction)onTap:(id)sender;
 
 
 @end
 
+
 @implementation NewsPostViewController
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -33,6 +40,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self registerForKeyboardNotifications];
     
     /// customize the post container view
     UIView *post = self.postContent;
@@ -45,6 +53,19 @@
     post.layer.borderColor = [UIColor colorWithWhite:0.0f alpha:.25].CGColor;
     
     
+    UIToolbar *cbar = self.commentToolbar;
+    
+    //create a text field and add it to the toolbar as a bar button item
+    UITextField *commentField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 220, 32)];
+    commentField.borderStyle = UITextBorderStyleRoundedRect;
+    commentField.background = [[UIImage alloc] initWithContentsOfFile:@"textfieldBackground"];
+    commentField.placeholder = @"Write a comment…";
+    self.commentField = commentField;
+    UIBarButtonItem *customFieldView = [[UIBarButtonItem alloc] initWithCustomView:commentField];
+    
+    
+    [cbar setItems:@[customFieldView] animated:YES];
+    
     // Do any additional setup after loading the view from its nib.
 }
 
@@ -54,4 +75,99 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.commentField endEditing:YES];
+}
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeShown:)
+                                                 name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+
+
+#pragma COMMENT BAR textfield stuffs
+
+CGRect _originalFrame;
+
+// Called when the UIKeyboardWillShowNotification is sent.
+- (void)keyboardWillBeShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    
+    // Get the duration of the animation.
+    NSValue *animationDurationValue = [info objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration;
+    [animationDurationValue getValue:&animationDuration];
+    
+    NSValue *kbcurve = [info objectForKey:UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve *curve;
+    [kbcurve getValue:&curve];
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    
+    CGRect aRect = self.commentToolbar.frame;
+    _originalFrame = aRect;
+    
+    aRect.origin.y = self.view.frame.size.height - kbSize.height - aRect.size.height;
+    
+    [UIView animateWithDuration:(animationDuration - .05f)
+                          delay:.05f
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         self.commentToolbar.frame = aRect;
+                     }
+                     completion:^(BOOL completed) {
+                         [self didFinishAnimatingField];
+                     }
+     ];
+    
+}
+
+- (void)didFinishAnimatingField
+{
+    //do stuff here when we're done
+}
+
+- (void)animateFieldwithKeyboardState:(BOOL)keyboardState {
+    
+
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    
+    
+    // Get the duration of the animation.
+    NSValue *animationDurationValue = [info objectForKey:UIKeyboardAnimationDurationUserInfoKey];
+    NSTimeInterval animationDuration;
+    [animationDurationValue getValue:&animationDuration];
+    
+    NSValue *kbcurve = [info objectForKey:UIKeyboardAnimationCurveUserInfoKey];
+    UIViewAnimationCurve *curve;
+    [kbcurve getValue:&curve];
+    
+    
+    [UIView animateWithDuration:animationDuration
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         self.commentToolbar.frame = _originalFrame;
+                     }
+                     completion:^(BOOL completed) {
+                         [self didFinishAnimatingField];
+                     }
+     ];
+}
 @end
